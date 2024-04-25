ef7db2b9cdef404c70fb92a491de3604dc534871
KandepiBhavani23 <kandepibhavani@gmail.com>
2024-04-25 19:52:07 +0530

    FrontEnd-Home

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,9 +1,13 @@
-import HeroSection from "@/components/Frontend/HeroSection";
+import HeroSection from "@/components/Frontend/HeroSection/HeroSection";
+import { MegaMenu } from "@/components/Frontend/MegaMenu";
+import TrustedCompanies from "@/components/Frontend/TrustedCompanies";
 
 const Home = () => {
   return (
     <section>
+      <MegaMenu />
       <HeroSection />
+      <TrustedCompanies />
     </section>
   );
 };
